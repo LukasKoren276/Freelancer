8cbc1a387f81e08596ddf1c18d10cbdab664e69e
LukasKoren276 <luk.koren@gmail.com>
2024-10-10 17:05:13 +0200

    settings window modified, db model user settings extended

--- Gui/settingsWindow.py
+++ Gui/settingsWindow.py
@@ -13,19 +13,30 @@ class SettingsWindow(ctk.CTkToplevel):
         super().__init__(parent)
         self.controller = controller
         self.title(window_title)
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=0)
+        self.grid_columnconfigure(2, weight=0, minsize=50)
+        self.grid_columnconfigure(3, weight=0)
+        self.grid_columnconfigure(4, weight=1)
+        self.grid_rowconfigure(0, minsize=50)
 
-        self.fields = {
+
+        self.common_fields = {
+            'company_name': (ctk.StringVar(), 'Company Name'),
+            'user_company_registration_number': (ctk.StringVar(), 'Company Registration Number'),
             'user_first_name': (ctk.StringVar(), 'First Name'),
             'user_last_name': (ctk.StringVar(), 'Last Name'),
             'user_street': (ctk.StringVar(), 'Street'),
             'user_street_number': (ctk.StringVar(), 'Street Number'),
             'user_city': (ctk.StringVar(), 'City'),
-            'user_country': (ctk.StringVar(), 'Country'),
-            'company_name': (ctk.StringVar(), 'Company Name'),
-            'user_company_registration_number': (ctk.StringVar(), 'Company Registration Number'),
-            'user_registered_as': (ctk.StringVar(), 'User Registered As'),
+            'user_country': (ctk.StringVar(), 'Country')
+        }
+
+        self.registed_as = {'user_registered_as': (ctk.StringVar(), 'User Registered As')}
+
+        self.others = {
+            'account_number': (ctk.StringVar(), 'Account Number'),
+            'bank_code': (ctk.StringVar(), 'Bank Code'),
             'invoice_due_date': (ctk.StringVar(), 'Invoice Due Date [days]'),
             'rate_per_hour': (ctk.StringVar(), 'Rate Per Hour'),
             'currency': (ctk.StringVar(), 'Currency'),
@@ -39,37 +50,57 @@ class SettingsWindow(ctk.CTkToplevel):
         user_settings = self.controller.get_user_settings()
 
         if user_settings:
-            for field_name, (var, _) in self.fields.items():
+            for field_name, (var, _) in self.common_fields.items():
                 value = getattr(user_settings, field_name, None)
 
                 if value is not None:
                     var.set(value)
 
     def create_window_objects(self) -> None:
-        for index, (name, (var, label_text)) in enumerate(self.fields.items()):
-            ctk.CTkLabel(
-                self,
-                text=label_text
-            ).grid(row=2 * index, column=1, padx=0, pady=0 if index != 0 else (20, 0), sticky='SW')
+        self.create_two_column_widgets(0, self.common_fields)
 
-            ctk.CTkEntry(
-                self,
-                textvariable=var,
-                width=300
-            ).grid(row=2 * index + 1, column=1, padx=0, pady=(0, 5), sticky='NW')
+        ctk.CTkLabel(
+            self,
+            text=self.registed_as['user_registered_as'][1]
+        ).grid(row=2 * len(self.common_fields) + 1, column=1, padx=0, pady=0, sticky='SW')
+
+        ctk.CTkEntry(
+            self,
+            textvariable=self.registed_as['user_registered_as'][0],
+            width=650
+        ).grid(row=2 * len(self.common_fields) + 2, column=1, padx=0, pady=(0, 15), sticky='NW', columnspan=3)
+
+        self.create_two_column_widgets(2 * len(self.common_fields) + 4, self.others)
 
         ctk.CTkButton(
             self,
             text='Save and Close',
             command=self.submit,
             font=ctk.CTkFont(family="Helvetica", size=15)
-        ).grid(row=2 * len(self.fields) + 1, column=1, pady=(20, 0))
+        ).grid(row=2 * len(self.common_fields) + 2 * len(self.others) + 1, column=1, pady=(20, 0), columnspan=3)
 
         WindowHelper.size_and_center(self, resiz=False, center=False)
 
+    def create_two_column_widgets(self, start: int, fields: dict):
+        for index, (name, (var, label_text)) in enumerate(fields.items()):
+            column = 1 if (index + start) % 2 == 0 else 3
+            row = ((index + start) // 2) * 2
+
+            ctk.CTkLabel(
+                self,
+                text=label_text
+            ).grid(row=row, column=column, padx=0, pady=0 if index + start != 0 else (20, 0), sticky='SW')
+
+            ctk.CTkEntry(
+                self,
+                textvariable=var,
+                width=300
+            ).grid(row=row + 1, column=column, padx=0, pady=(0, 15), sticky='NW')
+
+
     def submit(self) -> None:
         user_settings = self.controller.get_user_settings()
-        validated_data = DataValidation.validate_data(UserSettings, self.fields)
+        validated_data = DataValidation.validate_data(UserSettings, self.common_fields)
 
         if validated_data is None:
             return
--- models/user_settings.py
+++ models/user_settings.py
@@ -6,28 +6,32 @@ class UserSettings(Base):
     __tablename__ = 'user_settings'
 
     id = Column(Integer, primary_key=True)
+    company_name = Column(String(255), nullable=True)
+    user_company_registration_number = Column(String(50), nullable=False)
     user_first_name = Column(String(50), nullable=False)
     user_last_name = Column(String(50), nullable=False)
     user_street = Column(String(100), nullable=False)
     user_street_number = Column(Integer, nullable=False)
     user_city = Column(String(50), nullable=False)
     user_country = Column(String(50), nullable=False)
-    company_name = Column(String(255), nullable=True)
-    user_company_registration_number = Column(String(50), nullable=False)
     user_registered_as = Column(String(255), nullable=False)
+    account_number = Column(String(20), nullable=False)
+    bank_code = Column(String(20), nullable=False)
     invoice_due_date = Column(Integer, nullable=False)
     rate_per_hour = Column(Integer, nullable=False)
     currency = Column(String(10), nullable=False)
     vat = Column(Integer, nullable=True)
 
     def __repr__(self):
-        return (f"<UserSettings(user_first_name={self.user_first_name}, "
-                f"user_last_name={self.user_last_name}, "
-                f"user_company_registration_number={self.user_company_registration_number}, "
-                f"user_street={self.user_street}, "
-                f"user_street_number={self.user_street_number}, "
-                f"user_city={self.user_city}, "
-                f"user_country={self.user_country}, "
-                f"user_registered_as={self.user_registered_as}, "
-                f"invoice_due_date={self.invoice_due_date}, "
-                f"currency={self.currency}, vat={self.vat})>")
+        return (f'<UserSettings(user_first_name={self.user_first_name}, '
+                f'user_last_name={self.user_last_name}, '
+                f'user_company_registration_number={self.user_company_registration_number}, '
+                f'user_street={self.user_street}, '
+                f'user_street_number={self.user_street_number}, '
+                f'user_city={self.user_city}, '
+                f'user_country={self.user_country}, '
+                f'user_registered_as={self.user_registered_as}, '
+                f'account_number={self.account_number} ',
+                f'bank_code={self.bank_code} ',
+                f'invoice_due_date={self.invoice_due_date}, '
+                f'currency={self.currency}, vat={self.vat})>')
